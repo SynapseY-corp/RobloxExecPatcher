5c8d8647e62b2f82f296390e06b675e0791b9c58
SynapseY-corp <s49037977+github@gmail.com>
2025-04-16 18:22:33 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+// Use if you want more functions than just settings.json
+
+var settings = {
+    // force macOs version of roblox (beta test) def. false
+    "force-macOs-version": false,
+    // should be enabled if you want use build-in executor
+    "EnableRbxMe": true,
+    // if enabled then window will pop-up with executor
+    "UseBuildInExecutor": false,
+    // same as luau-force-version, setting version from 2022 (MAY CAUSE ERRORS IN SCRIPTS)
+    "Use2022luau": false,
+    // forcing luau version def. null (null = not forcing verison)
+    "luau-force-version": null
+}
